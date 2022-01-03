fcc953b310ef8c403434c341ae692766d46789e3
gopi <gopirm02@gmail.com>
2022-01-03 15:59:13 +0530

    init

---
+++ frontend/src/app/listing/listing-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AllListingComponent } from './all-listing/all-listing.component';
+import { ListingDetailComponent } from './listing-detail/listing-detail.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: AllListingComponent
+  },
+  {
+    path: ":id",
+    component: ListingDetailComponent
+  }
+ 
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ListingRoutingModule { }
---
+++ frontend/src/app/listing/service/listing.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, observable } from 'rxjs';
+import { Listing } from '../model/listing';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ListingService {
+  private ROOT_URL = "http://localhost:4000/api/listings";
+
+  constructor(private http: HttpClient) { }
+
+  getListings(): Observable <Listing[]>{
+    return this.http.get<Listing[]>(this.ROOT_URL);
+  }
+
+  getListing(id:any) {
+    return this.http.get<Listing>(`${this.ROOT_URL}/${id}`);
+  }
+
+  
+}
